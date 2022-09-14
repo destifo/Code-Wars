7289ac0e928229a320485551dd690b995243eaf1
destifo <estifanosbireda@gmail.com>
2022-09-14 11:06:09 +0300

    added a shorter solution for String incrementer

--- String incrementer/IncrementString.py
+++ String incrementer/IncrementString.py
@@ -35,3 +35,9 @@ def increment_string(strng):
     
     new_strng = strng[:m-n] + str(inc_num)
     return new_strng
+
+def increment_string(strng):
+    head = strng.rstrip('0123456789')
+    tail = strng[len(head):]
+    if tail == "": return strng+"1"
+    return head + str(int(tail) + 1).zfill(len(tail))
